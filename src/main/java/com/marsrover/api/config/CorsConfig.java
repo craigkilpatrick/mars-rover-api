9872ec3e08f04bbc3e19e84b3d674e039bf903ac
Craig Kilpatrick <craig.kilpatrick@liatrio.com>
2025-03-10 19:52:28 -0500

    fix: resolving non-null error

--- src/main/java/com/marsrover/api/config/CorsConfig.java
+++ src/main/java/com/marsrover/api/config/CorsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
 
 @Configuration
 public class CorsConfig {
@@ -11,7 +12,7 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**") // Apply to all endpoints
                         .allowedOrigins("http://localhost:3000") // Allow frontend requests
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
